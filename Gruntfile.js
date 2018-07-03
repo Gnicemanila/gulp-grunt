389523db31bf5a03c59d06668376043ce3e04c44
nice <nicemanila@gmail.com>
2018-07-03 13:45:10 +0800

    配置压缩

---
+++ Gruntfile.js
@@ -0,0 +1,51 @@
+﻿
+module.exports = function(grunt) {
+
+  // 项目配置信息
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      uglify_grunt: {
+        options: {
+          sourceMap: false,
+          output: {
+            quote_style: 3
+          }
+        },
+        files: [{
+          expand: true,
+
+
+          //路径写对
+          // src: ['ssc/js/game/es5/*.js', 'sscmobile/js/game/es5/*.js'],
+          src: ['ssc/js/game/es5/*.js'],
+          //src: ['sscmobile/js/game/es5/*.js'],
+
+
+
+
+
+
+
+
+          dest: '',
+          cwd: '.',
+          rename: function(dst, src) {
+            // To keep the source js files and make new files as `*.min.js`:
+            // return dst + '/' + src.replace('.js', '.min.js');
+            // Or to override to src:
+            // return src;
+            return src.replace('es5', 'min').replace('.js', '.min.js');
+          }
+        }]
+      }
+    },
+  });
+
+  // 加载插件
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  // 执行任务，任务名称是default
+  grunt.registerTask('default', ['uglify']);
+
+};
---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+﻿
+var gulp = require("gulp");
+var babel = require("gulp-babel");
+var utf8Convert = require('gulp-utf8-convert');
+
+
+// 压缩PC端所有js
+// gulp
+gulp.task("default", function() {
+  return gulp.src("ssc/js/game/source/*.js") // ES6 源码存放的路径
+
+    .pipe(utf8Convert({
+      encNotMatchHandle: function(file) {
+        console.log(file + " 编码错误，已经修改！");
+      }
+    }))
+
+    .pipe(babel())
+    .pipe(gulp.dest("ssc/js/game/es5")); //转换成 ES5 存放的路径
+});
+
+
+
+
+
+// 压缩移动端所有js
+// gulp mobile
+gulp.task("mobile", function() {
+  return gulp.src("sscmobile/js/game/source/*.js") // ES6 源码存放的路径
+    .pipe(utf8Convert({
+      encNotMatchHandle: function(file) {
+        console.log(file + " 编码错误，已经修改！");
+      }
+    }))
+    .pipe(babel())
+    .pipe(gulp.dest("ssc/js/game/es5")); //转换成 ES5 存放的路径
+});
+
+
+
+
+
+
+// 压缩单个js
+// gulp jslhc
